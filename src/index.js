1424d34b3e75286c941f493737ed16c7ec16b623
MohamedSy1 <mohamed.sy@compscihigh.org>
2023-10-30 20:01:19 -0400

    created a basic CLI  that lets u take in user input and multiply it

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log('hello')
+const prompt = require('prompt-sync')();
+
+const askYourName = () => {
+    const name = prompt("what is your name? ")
+    return `hello ${name} `
+
+}
+
+const timesTwo = () => {
+    const num = prompt(`How many more times? `)
+    console.log(`Your num multiplied by two is: ${num * 2}`)
+}
+
+
+const main = () => {
+    askYourName();
+    timesTwo();
+}
+
+main()
